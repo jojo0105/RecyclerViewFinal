10fa101442d342bc9c0583294a8fb73a6c2d5af2
George Maged Samir Aziz <2070513@edu.vaniercollege.qc.ca>
2022-03-02 14:55:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+
+    String[] monthNames = {
+            "January", "February", "March", "April", "May", "June", "July", "August",
+            "September", "October", "November", "December"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerAdapter(this, monthNames);
+        recyclerView.setAdapter(adapter);
+    }
+}
